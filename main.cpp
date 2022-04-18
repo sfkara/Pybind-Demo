5f3e9d747ce0d51d44dfa2be2780c22f6ff7b6ad
sfkara <seriffiratkara@gmail.com>
2022-04-18 10:25:57 +0300

    add new class and funcs with test

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 using namespace std;
 
+namespace py = pybind11;
+
 float some_fn(float a, float b)
 {
     return a + b;
@@ -20,8 +22,36 @@ int some_fn_2()
     return 0;
 }
 
+class SomeClass
+{
+    float multiplier;
+
+public:
+    SomeClass(float multiplier_) : multiplier(multiplier_){};
+
+    float multiply(float input)
+    {
+        return multiplier * input;
+    }
+    // Accept vector as a input, returns list
+    std::vector<float> multiply_list(std::vector<float> items)
+    {
+
+        for (auto i = 0; i < items.size(); i++)
+        {
+            items[i] = multiply(items[i]);
+        }
+        return items;
+    }
+};
+
 PYBIND11_MODULE(module_name, handle)
 {
     handle.doc() = "tadaaa";
     handle.def("some_fn_2", &some_fn_2);
+
+    py::class_<SomeClass>(handle, "PyClass")
+        .def(py::init<float>())
+        .def("multiply", &SomeClass::multiply)
+        .def("multiply_list", &SomeClass::multiply_list);
 }
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import module_name
+
 from time import perf_counter
 
 # t2_start = perf_counter()
@@ -6,23 +7,33 @@ from time import perf_counter
 # t2_stop = perf_counter()
 
 
-def pfor():
-    for i in range(5000):
-        for j in range(50):
-            print(i, j)
-    return 0
+# def pfor():
+#     for i in range(5000):
+#         for j in range(50):
+#             print(i, j)
+#     return 0
 
 
-t1_start = perf_counter()
-print(pfor())
-t1_stop = perf_counter()
-print("Elapsed time:", t1_stop, t1_start)
+# t1_start = perf_counter()
+# print(pfor())
+# t1_stop = perf_counter()
+# print("Elapsed time:", t1_stop, t1_start)
 
 
-print("Elapsed time during the whole program in seconds:", t1_stop - t1_start)
+# print("Elapsed time during the whole program in seconds:", t1_stop - t1_start)
 
 
 # print("Elapsed time  2:", t2_stop, t2_start)
 
 
 # print("Elapsed time during the whole program in seconds   2:", t2_stop - t2_start)
+
+
+# print(dir(module_name))
+
+m = module_name.PyClass(5)
+
+print(m.multiply(5))
+
+
+print(m.multiply_list([1, 2, 3, 4, 5]))
